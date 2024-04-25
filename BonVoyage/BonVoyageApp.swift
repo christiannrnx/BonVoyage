f6fa3479d3e86dbbda8a51f42d1a5b2ff953eb40
Christian Romero <christianrne@gmail.com>
2024-04-25 23:09:11 +0200

    request user location

--- BonVoyage/BonVoyageApp.swift
+++ BonVoyage/BonVoyageApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct BonVoyageApp: App {
+    
+    @StateObject var locationManager = LocationManager()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(locationManager)
         }
     }
 }
---
+++ BonVoyage/Features/Manager/LocationManager.swift
@@ -0,0 +1,67 @@
+//
+//  LocationManager.swift
+//  BonVoyage
+//
+//  Created by Christian Romero on 25/4/24.
+//
+
+import Foundation
+import MapKit
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    // Variable that manages the location, start and stop delivery of location related events
+    private var locationManager: CLLocationManager?
+    
+    @Published var region: MKCoordinateRegion?
+    @Published var location: CLLocation??
+    @Published var error: Error?
+    
+    override init() {
+        super.init()
+        requestLocation()
+    }
+    
+    // Function that requests the user to access their location
+    func requestLocation(){
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        // Best location accuracy if possible
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    // Function that determines when to request location
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        switch status {
+            case .notDetermined:
+                locationManager?.requestWhenInUseAuthorization()
+                break
+            default:
+                locationManager?.requestLocation()
+                break
+        }
+    }
+    
+    // Function to update the location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // We work with the last location
+        if let location = locations.last {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            self.region = region
+            self.location = location
+        }
+    }
+    
+    // Function to handle errors
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.error = error
+    }
+    
+    
+    
+    
+    
+}
